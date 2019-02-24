42063e946fdc333be326d305469d390c8d192b46
Marco David Martinez <markwinap@gmail.com>
2019-02-23 23:13:22 -0600

    Added Gamepad Support

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 Marco Martinez
 */
 
+
+//GAMEPÄD
+const gamepad = require('gamepad');
 //CONSOLE COLORS
 const colors = require('colors');
 //EXPRESS HTTP SERVER
@@ -24,12 +27,79 @@ const port = 8889;//TELLO PORT
 const port_status = 8890;//TELLO STATUS PORT
 //LEVEL CMD Desired Height
 const level_height = 110;
-
 //DRONE DISTANCE
 const drone_dist = 20;
-
 //DRON THROW TAKEOFF Z FORCE
 const throw_force = 50;
+
+
+//GAMEPAD STUFF
+gamepad.init();
+const tello_gamepad = '192.168.10.1';
+const deathZone = 0.099;//FILTER FOR AXIS
+const scanInterval = 15;//Miliseconds to scan controller input
+const getInputInterval = 300;//Miliseconds to send comand to drone
+let controllerType = 'ps4';
+
+let controller = {//Controller mapping
+    xbox_1 : {
+        axis: {
+            0: {d: 1},// - 1 to invert result
+            1: {c: 1},
+            2: {a: 1},
+            3: {b: 1}
+        },
+        button: {
+            4: 'land',
+            5: 'takeoff',
+            0: 'flip f',
+            1: 'flip b',
+            2: 'flip l',
+            3: 'flip r',
+            6: 'emergency',
+            7: 'emergency',
+            8: 'command',
+            9: 'command',
+            10: 'flip b',
+            11: 'flip r',
+            12: 'flip l',
+            13: 'flip f',
+        },
+        button_exclude : [],
+        axis_exclude : [4, 5]
+    },
+    ps4 : {
+        axis: {
+            0: {b: -1},// - 1 to invert result
+            1: {a: 1},
+            2: {c: -1},
+            3: {d: 1}
+        },
+        button: {
+            4: 'command',
+            5: 'command',
+            0: 'flip l',
+            1: 'flip b',
+            2: 'flip r',
+            3: 'flip f',
+            6: 'command',
+            7: 'command',
+            8: 'takeoff',
+            9: 'land',
+            10: 'emergency',
+            11: 'emergency',
+            12: 'command',
+            13: 'command',
+        },
+        button_exclude : [],
+        axis_exclude : [4, 5, 6, 7]
+    }
+};
+let temp_input = {
+    axis: {},
+    button: {}
+}
+
 
 let commands = {};//OBJECT STATS AND COMMANDS HOLDER
 
@@ -52,7 +122,7 @@ server.on('error', (err) => {
 server.on('message', (msg, rinfo) => {
   //UNCOMNET FOR DEBUG
   console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-  nextCMD(rinfo.address);
+  nextCMD(rinfo.address);//Check if commands available 
 });
 server.on('listening', () => {
   let address = server.address();
@@ -636,13 +706,19 @@ function senCMD(tello, command) {//SEND COMMAND TO TELLO OR SPECIAL FUNCTIONS
 }
 function nextCMD(tello) {//GET NEXT COMMAND IN LINE
   if (commands.hasOwnProperty(tello)) {
-    let cmd = commands[tello]['cmd_list'];
-    if (cmd.length > 1) {
-      cmd.shift();
-      senCMD(tello, commands[tello]['cmd_list'][0]);
-    } else {
+    if(commands[tello].hasOwnProperty('cmd_list')){
+      let cmd = commands[tello]['cmd_list'];
+      if (cmd.length > 1) {
+        cmd.shift();
+        senCMD(tello, commands[tello]['cmd_list'][0]);
+      } else {
+        console.log('ALL CMD SENT');
+      }
+    }
+    else{
       console.log('ALL CMD SENT');
     }
+
   } else {
     console.log('Drone Not Found');
   }
@@ -703,4 +779,78 @@ function getCCW(a, b){
     }
   }
   return res;
+}
+
+
+//###GAMEPAD FUNCTIONS
+let device = gamepad.deviceAtIndex();
+controllerType = getVendor(device);
+
+let getControllers = setInterval(gamepad.detectDevices, 500);
+let getButtons = setInterval(gamepad.processEvents, scanInterval);
+let getInput = setInterval(function(){
+    let buttons = Object.keys(temp_input.button);
+    for(let i in buttons){
+        if(temp_input.button[buttons[i]]){
+            sendCMD(controller[controllerType].button[buttons[i]]);
+        }
+    }
+    sendCMD(getRC(temp_input.axis, controller[controllerType].axis));
+    temp_input.button = {};
+}, getInputInterval);
+
+//###GAMEPAD EVENT LISTENER
+gamepad.on('move', function (id, axis, value) {
+  if(value < (deathZone * -1) || value > deathZone) {
+    if(!controller[controllerType].axis_exclude.includes(axis)){
+        temp_input.axis[axis] = value;
+    }
+  }
+  else{
+    if(!controller[controllerType].axis_exclude.includes(axis)){
+        temp_input.axis[axis] = false;
+    }    
+  }
+});
+gamepad.on('down', function (id, num) {    
+    if(!controller[controllerType].button_exclude.includes(num)){
+        temp_input.button[num] = true;
+    }
+});
+gamepad.on('attach', function (id, device) {
+    controllerType = getVendor(device);
+});
+function sendCMD(command){
+  return new Promise((resolve, reject) => {
+      let msg = Buffer.from(command);
+      server.send(msg, 0, msg.length, port, tello_gamepad, function (err) {
+        if (err) {
+          console.error(err);
+          reject(`ERROR : ${command}`);
+        } else resolve('OK');
+      });
+    });
+}
+function getRC(axis, axis_map){
+  let obj = {a: 0, b: 0, c: 0, d: 0};
+  let axis_arr = Object.keys(axis);//{ '0': false, '1': false, '2': false, '3': false }
+  for(let i in axis_arr){
+      let temp = Object.keys(axis_map[axis_arr[i]]);//{d: 1}
+      obj[temp[0]] = axis[axis_arr[i]] ? parseInt((axis[axis_arr[i]] * 100) * axis_map[axis_arr[i]][temp[0]], 0) : 0;        
+  }
+  return `rc ${obj.a} ${obj.b} ${obj.c} ${obj.d}`;    
+}
+function getVendor(device){
+  let controllerType = '';
+  if(device.vendorID == '1118'){
+      controllerType =  'xbox_1';
+  }
+  else if(device.vendorID == '1356'){
+      controllerType =  'ps4';
+  }
+  else{
+      controllerType = 'ps4';
+  }
+  console.log(`YOU CONNECTED A ${controllerType} CONTROLLER`);
+  return controllerType;
 }
